32b35d4146af3b56470e29037532fa86e2946a5f
dorcasChero <dorcas@sendyit.com>
2025-07-09 13:33:54 +0300

    dockerise parking service

---
+++ parking-service/src/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5002;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Middleware
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send(" Parking Service is alive!");
+});
+
+// MongoDB connection
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB...");
+    app.listen(PORT, () => {
+      console.log(`Parking Service running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
+
+
